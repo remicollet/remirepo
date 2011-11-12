462eb7c582cfc74442966db848e811b06d59c998
Remi Collet <fedora@famillecollet.com>
2011-11-12 08:50:45 +0100

    Xulrunner 8.0

---
+++ xulrunner8/xulrunner-redhat-default-prefs.js
@@ -0,0 +1,6 @@
+pref("app.update.enabled", false);
+pref("app.update.autoInstallEnabled", false);
+pref("general.smoothScroll",                true);
+pref("intl.locale.matchOS", true);
+pref("network.manage-offline-status", true);
+pref("toolkit.storage.synchronous", 0);
