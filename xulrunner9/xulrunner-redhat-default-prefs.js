8db0a410706cda85ca7f15f3276945d70a8321a2
Remi Collet <fedora@famillecollet.com>
2011-12-22 16:25:40 +0100

    clean layout

--- xulrunner9/xulrunner-redhat-default-prefs.js
+++
@@ -1,6 +0,0 @@
-pref("app.update.enabled", false);
-pref("app.update.autoInstallEnabled", false);
-pref("general.smoothScroll",                true);
-pref("intl.locale.matchOS", true);
-pref("network.manage-offline-status", true);
-pref("toolkit.storage.synchronous", 0);
