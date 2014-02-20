59e69d60d1c6f126a6a1f67c798d3193fbb87b28
Remi Collet <fedora@famillecollet.com>
2014-02-20 11:50:10 +0100

    php-horde-Horde-Autoloader: add test for autoloader and Sabre

---
+++ php/horde/php-horde-Horde-Autoloader/test.php
@@ -0,0 +1,9 @@
+<?php
+require "Horde/Autoloader/Default.php";
+
+echo "Sabre_CalDAV:    " . Sabre\CalDAV\Version::VERSION . "\n";
+echo "Sabre_CardDAV:   " . Sabre\CardDAV\Version::VERSION . "\n";
+echo "Sabre_DAV:       " . Sabre\DAV\Version::VERSION . "\n";
+echo "Sabre_DAVACL:    " . Sabre\DAVACL\Version::VERSION . "\n";
+echo "Sabre_HTTP:      " . Sabre\HTTP\Version::VERSION . "\n";
+echo "Sabre_VObject:   " . Sabre\VObject\Version::VERSION . "\n";
