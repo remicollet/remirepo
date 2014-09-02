0732592b015a026761ead0a8fea51cef78aae093
Remi Collet <fedora@famillecollet.com>
2014-09-02 18:24:41 +0200

    php55-php: import from rhscl 1.1

---
+++ scl-php55/php/macros.php
@@ -0,0 +1,15 @@
+#
+# Interface versions exposed by PHP:
+# 
+%php55_php_core_api @PHP_APIVER@
+%php55_php_zend_api @PHP_ZENDVER@
+%php55_php_pdo_api  @PHP_PDOVER@
+%php55_php_version  @PHP_VERSION@
+
+%php55_php_extdir   @LIBDIR@/php/modules
+
+%php55_php_inidir   @ETCDIR@/php.d
+
+%php55_php_incldir  @INCDIR@/php
+
+%php55___php        @BINDIR@/php
