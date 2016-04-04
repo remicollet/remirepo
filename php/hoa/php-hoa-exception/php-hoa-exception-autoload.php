7e05caae92173bdf9bf49d6677497e6b42f4423b
Remi Collet <fedora@famillecollet.com>
2016-04-04 19:26:48 +0200

    php-hoa-exception: use Hoa autoloader

--- php/hoa/php-hoa-exception/php-hoa-exception-autoload.php
+++ php/hoa/php-hoa-exception/php-hoa-exception-autoload.php
@@ -1,24 +1,13 @@
 <?php
-/* Autoloader for hoa/event and its dependencies */
+/* Autoloader for hoa/exception and its dependencies */
 
 $vendor = '/usr/share/php';
-// Use Symfony autoloader
-if (!isset($fedoraClassLoader) || !($fedoraClassLoader instanceof \Symfony\Component\ClassLoader\ClassLoader)) {
-    if (!class_exists('Symfony\\Component\\ClassLoader\\ClassLoader', false)) {
-        require_once $vendor . '/Symfony/Component/ClassLoader/ClassLoader.php';
-    }
-
-    $fedoraClassLoader = new \Symfony\Component\ClassLoader\ClassLoader();
-    $fedoraClassLoader->register();
-}
-
-$fedoraClassLoader->addPrefix('Hoa\\Exception\\', dirname(dirname(__DIR__)));
-
 // Dependencies
 foreach ([
 	$vendor . '/Hoa/Consistency/autoload.php'    => true,
 	$vendor . '/Hoa/Event/autoload.php'          => true,
 	] as $dep => $mandatory) {
-	if ($mandory || file_exists($dep) require_once($dep);
+	if ($mandatory || file_exists($dep)) require_once($dep);
 }
+$fedoraHoaLoader->addNamespace('Hoa\\Exception\\', __DIR__, true);
 
