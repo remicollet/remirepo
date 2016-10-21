e3a4e441ac9845b1e8cac8b2ef18f97907685dc9
Remi Collet <fedora@famillecollet.com>
2016-10-21 11:01:12 +0200

    php-composer: minor cleanup

--- php/php-composer-ca-bundle/php-composer-ca-bundle-autoload.php
+++ php/php-composer-ca-bundle/php-composer-ca-bundle-autoload.php
@@ -1,7 +1,6 @@
 <?php
 /* Autoloader for composer/ca-bundle and its dependencies */
 
-$vendorDir = '/usr/share/php';
 if (!class_exists('Fedora\\Autoloader\\Autoload', false)) {
     require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
 }
--- php/php-composer-spdx-licenses/php-composer-spdx-licenses-autoload.php
+++ php/php-composer-spdx-licenses/php-composer-spdx-licenses-autoload.php
@@ -1,7 +1,6 @@
 <?php
 /* Autoloader for composer/spdx-licenses and its dependencies */
 
-$vendorDir = '/usr/share/php';
 if (!class_exists('Fedora\\Autoloader\\Autoload', false)) {
     require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
 }
