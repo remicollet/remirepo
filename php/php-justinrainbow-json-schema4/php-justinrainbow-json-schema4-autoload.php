14229b9d2e4b8b3c2a81174fbfa7db4fcf8f40fa
Remi Collet <fedora@famillecollet.com>
2016-12-12 10:28:54 +0100

    php-justinrainbow-json-schema4: 4.0.1 (new package)

---
+++ php/php-justinrainbow-json-schema4/php-justinrainbow-json-schema4-autoload.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Autoloader for justinrainbow/json-schema and its dependencies
+ */
+
+require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
+
+\Fedora\Autoloader\Autoload::addPsr4('JsonSchema\\', __DIR__);
