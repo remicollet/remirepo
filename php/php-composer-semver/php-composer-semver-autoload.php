c8b3eeb0e61caf92f672958e09811d7466c22e02
Remi Collet <fedora@famillecollet.com>
2017-03-24 14:45:33 +0100

    php/php-composer-semver moved to git.remirepo.net

--- php/php-composer-semver/php-composer-semver-autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Autoloader for composer/semver and its dependencies */
-
-if (!class_exists('Fedora\\Autoloader\\Autoload', false)) {
-    require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
-}
-
-\Fedora\Autoloader\Autoload::addPsr4('Composer\\Semver\\', __DIR__);
-
