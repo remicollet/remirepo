d708e5fb6028c0d8b20542532459dc3494df3182
Remi Collet <fedora@famillecollet.com>
2017-03-24 14:30:43 +0100

    php/php-mongodb moved to git.remirepo.net

--- php/php-mongodb/php-mongodb-autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* Autoloader for mongodb/mongodb and its dependencies */
-
-require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
-\Fedora\Autoloader\Autoload::addPsr4('MongoDB\\', __DIR__);
-require_once __DIR__. '/functions.php';
