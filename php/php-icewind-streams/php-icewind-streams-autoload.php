a688d143e7a00423c1a14e1d2e55fe679f3ff18e
Remi Collet <fedora@famillecollet.com>
2017-03-24 14:38:09 +0100

    php/php-icewind-streams moved to git.remirepo.net

--- php/php-icewind-streams/php-icewind-streams-autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Autoloader for icewind/streams and its dependencies */
-
-require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
-
-\Fedora\Autoloader\Autoload::addPsr4('Icewind\\Streams\\', __DIR__);
-
