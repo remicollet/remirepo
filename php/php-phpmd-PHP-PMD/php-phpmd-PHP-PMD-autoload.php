4568f7b494efb2f5300bfb03be19a1eec2f27a62
Remi Collet <fedora@famillecollet.com>
2017-03-24 14:33:09 +0100

    php/php-phpmd-PHP-PMD moved to git.remirepo.net

--- php/php-phpmd-PHP-PMD/php-phpmd-PHP-PMD-autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Autoloader for pdepend/pdepend and its dependencies */
-
-require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
-\Fedora\Autoloader\Autoload::addPsr4('PHPMD\\', __DIR__);
-\Fedora\Autoloader\Dependencies::required(array(
-	'/usr/share/php/PDepend/autoload.php',
-));
-
