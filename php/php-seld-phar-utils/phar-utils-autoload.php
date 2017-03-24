c4a4d8c14ab5d5f473b626e4ca9d8482260aa00b
Remi Collet <fedora@famillecollet.com>
2017-03-24 14:24:22 +0100

    php/php-seld-phar-utils moved to git.remirepo.net

--- php/php-seld-phar-utils/phar-utils-autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Autoloader for seld/phar-utils and its dependencies */
-
-if (!class_exists('Fedora\\Autoloader\\Autoload', false)) {
-    require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
-}
-
-\Fedora\Autoloader\Autoload::addPsr4('Seld\\PharUtils\\', __DIR__);
-
