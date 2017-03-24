61f128d1cec2c9443462d865ae1497a33afdbd59
Remi Collet <fedora@famillecollet.com>
2017-03-24 14:24:18 +0100

    php/php-seld-cli-prompt moved to git.remirepo.net

--- php/php-seld-cli-prompt/cli-prompt-autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Autoloader for seld/cli-prompt and its dependencies */
-
-if (!class_exists('Fedora\\Autoloader\\Autoload', false)) {
-    require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
-}
-
-\Fedora\Autoloader\Autoload::addPsr4('Seld\\CliPrompt\\', __DIR__);
-
