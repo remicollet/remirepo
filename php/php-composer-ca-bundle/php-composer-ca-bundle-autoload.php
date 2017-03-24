b94c84bf80dbec8fe79c8282680f1935154d6b1e
Remi Collet <fedora@famillecollet.com>
2017-03-24 14:45:25 +0100

    php/php-composer-ca-bundle moved to git.remirepo.net

--- php/php-composer-ca-bundle/php-composer-ca-bundle-autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Autoloader for composer/ca-bundle and its dependencies */
-
-require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
-
-\Fedora\Autoloader\Autoload::addPsr4('Composer\\CaBundle\\', __DIR__);
-
