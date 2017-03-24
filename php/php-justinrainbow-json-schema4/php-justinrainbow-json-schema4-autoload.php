92200ce490fb657cf4d76b7369db13120c42b01c
Remi Collet <fedora@famillecollet.com>
2017-03-24 14:39:16 +0100

    php/php-justinrainbow-json-schema4 moved to git.remirepo.net

--- php/php-justinrainbow-json-schema4/php-justinrainbow-json-schema4-autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Autoloader for justinrainbow/json-schema and its dependencies
- */
-
-require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
-
-\Fedora\Autoloader\Autoload::addPsr4('JsonSchema\\', __DIR__);
