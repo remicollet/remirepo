7aebc893e35d8abc2d55bb79c4d501fb8ec9b633
Remi Collet <fedora@famillecollet.com>
2017-03-24 07:08:00 +0100

    php/sabredav/php-sabre-event moved to git.remirepo.net

--- php/sabredav/php-sabre-event/php-sabre-event-autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Autoloader for sabre/event and its dependencies */
-
-require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
-
-\Fedora\Autoloader\Autoload::addPsr4('Sabre\\Event\\', __DIR__);
-
