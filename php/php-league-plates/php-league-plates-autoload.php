ed657b58956e81e7165268a772841c1465ac1462
Remi Collet <fedora@famillecollet.com>
2017-03-24 14:39:31 +0100

    php/php-league-plates moved to git.remirepo.net

--- php/php-league-plates/php-league-plates-autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Autoloader for league/plates and its dependencies */
-
-require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
-
-\Fedora\Autoloader\Autoload::addPsr4('League\\Plates\\', __DIR__);
-
