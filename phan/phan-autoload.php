5673829ad19bf4d55ca04869fd4c10741a5eff50
Remi Collet <fedora@famillecollet.com>
2017-03-24 12:40:30 +0100

    phan moved to git.remirepo.net

--- phan/phan-autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Autoloader for etsy/phan and its dependencies */
-
-require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
-
-\Fedora\Autoloader\Autoload::addPsr4('Phan\\', __DIR__);
-\Fedora\Autoloader\Dependencies::required(array(
-    '/usr/share/php/Symfony/Component/Console/autoload.php',
-));
