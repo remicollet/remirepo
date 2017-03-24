34ecd2eab448ebe3f4d7a8dbed19e7e9db8e71b1
Remi Collet <fedora@famillecollet.com>
2017-03-24 14:32:35 +0100

    php/php-pdepend-PHP-Depend moved to git.remirepo.net

--- php/php-pdepend-PHP-Depend/php-pdepend-PHP-Depend-autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Autoloader for pdepend/pdepend and its dependencies */
-
-require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
-\Fedora\Autoloader\Autoload::addPsr4('PDepend\\', __DIR__);
-\Fedora\Autoloader\Dependencies::required(array(
-	'/usr/share/php/Symfony/Component/autoload.php',
-));
-
